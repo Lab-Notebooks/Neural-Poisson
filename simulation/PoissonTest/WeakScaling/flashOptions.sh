9648e1d224a4f5f5bf689e35d896e4db52a6e78b
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-11-01 23:45:20 -0500

    Update WeakScaling test

--- simulation/PoissonTest/WeakScaling/flashOptions.sh
+++ simulation/PoissonTest/WeakScaling/flashOptions.sh
@@ -1,4 +1,4 @@
 # cache the value of current working directory
 
-FlashOptions="unitTest/Poisson/Amrex -auto -maxblocks=100 +amrex +parallelIO \
-   	      -site=$SiteHome IOWriteGridFiles=True -3d -nxb=16 -nyb=16 -nyb=16"
+FlashOptions="unitTest/Poisson/General -auto -maxblocks=100 +amrex +parallelIO \
+   	      -site=$SiteHome IOWriteGridFiles=True -3d -nxb=16 -nyb=16 -nzb=16"
